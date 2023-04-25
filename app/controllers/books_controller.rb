eae7d78d9668a13b2eb0717a41fb7c45f49b7779
shiitakedaisuki <129662461+shiitakedaisuki@users.noreply.github.com>
2023-04-25 13:53:00 +0000

    Finish1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,28 +10,45 @@ class BooksController < ApplicationController
   def create
     @post_book = Book.new(post_book_params)#()の中にもストロングパラメーターを入れる。脆弱性を補う
     @post_book.user_id = current_user.id
-    @post_book.save
-    redirect_to book_path(@post_book.id)#booksのshow画面に飛ぶ
+    if @post_book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@post_book.id)#booksのshow画面に飛ぶ
+    else
+      @books = Book.all
+      @user = current_user
+      render :index #(render :の後はアクション名を入れる)
+    end
   end
   
   def index
     @books = Book.all
     @post_book = Book.new
+    @user = current_user
   end
   
   def show
     @post_book = Book.new
     @book = Book.find(params[:id])
+    @user = @book.user
   end
   
   def edit
     @book = Book.find(params[:id])
+      if @book.user == current_user
+        render "edit"
+      else
+        redirect_to books_path
+      end
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(post_book_params)#()の中にもストロングパラメーターを入れる
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(post_book_params)#()の中にもストロングパラメーターを入れる
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit #(render :の後はアクション名を入れる)
+    end
   end
   
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,19 +4,41 @@ class UsersController < ApplicationController
   
   def index
     @users = User.all
+    @post_book = Book.new
+    @user = current_user
   end
   
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @post_book = Book.new
   end
   
   def edit
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
     @user = User.find(params[:id])
   end
   
   def update
-    user = User.find(params[:id])
-    redirect_to user_path(user.id)
+      user = User.find(params[:id])
+      unless user.id == current_user.id
+        redirect_to post_images_path
+      end
+    @user = User.find(params[:id])
+    if @user.update(user_params)#()の中にもストロングパラメーターを入れる
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:introduction, :name, :profile_image)#params.require(:モデルのファイル名)
   end
 end
